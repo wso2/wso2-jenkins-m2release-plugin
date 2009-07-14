0382bbd8ab37d4ce24dabcdfc2c5c02dfe83048f
teilo <teilo@dev.java.net>
2009-07-14 14:23:34 +0000

    add nexus staging support - (part one - breaks the build)

--- src/main/java/org/jvnet/hudson/plugins/m2release/M2ReleaseBuildWrapper.java
+++ src/main/java/org/jvnet/hudson/plugins/m2release/M2ReleaseBuildWrapper.java
@@ -34,18 +34,25 @@ import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.Action;
 import hudson.model.BuildListener;
-import hudson.model.Item;
+import hudson.model.Hudson;
+import hudson.model.Job;
+import hudson.security.Permission;
 import hudson.tasks.BuildWrapper;
 import hudson.tasks.BuildWrapperDescriptor;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.sonatype.nexus.restlight.common.RESTLightClientException;
+import org.sonatype.nexus.restlight.stage.StageClient;
+import org.sonatype.nexus.restlight.stage.StageRepository;
 
 /**
  * Wraps a {@link MavenBuild} to be able to run the 
- * <a href="http://maven.apache.org/plugins/maven-release-plugin/">maven release plugin</a> on demand.
+ * <a href="http://maven.apache.org/plugins/maven-release-plugin/">maven release plugin</a> on demand, 
+ * with the ability to auto close a Nexus Pro Staging Repo
  * 
  * @author James Nord
  * @version 0.2
@@ -54,11 +61,16 @@ import org.kohsuke.stapler.DataBoundConstructor;
 public class M2ReleaseBuildWrapper extends BuildWrapper {
 
 	private transient boolean doRelease = false;
+	private transient boolean closeNexusStage = true;
 	private transient Map<String,String> versions;
 	private transient boolean appendHudsonBuildNumber;
-
+	
 	public String releaseGoals = DescriptorImpl.DEFAULT_RELEASE_GOALS;
-
+	public boolean nexusStagingSupport = true;
+	public String nexusURL = DescriptorImpl.DEFAULT_NEXUS_URL;
+	public String nexusUser = DescriptorImpl.DEFAULT_NEXUS_USER;
+	public String nexusPassword = DescriptorImpl.DEFAULT_NEXUS_PASSWORD;
+	
 	@DataBoundConstructor
 	public M2ReleaseBuildWrapper(String releaseGoals) {
 		super();
@@ -109,17 +121,48 @@ public class M2ReleaseBuildWrapper extends BuildWrapper {
 			
 			@Override
 			public boolean tearDown(AbstractBuild bld, BuildListener lstnr) throws IOException,
-			                                                                    InterruptedException {
+			                                                                       InterruptedException {
 				// TODO only re-set the build goals if they are still releaseGoals to avoid mid-air collisions.
 				boolean retVal = true;
-				MavenModuleSet mmSet = getModuleSet(bld); 
+				MavenModuleSet mmSet = getModuleSet(bld);
 				mmSet.setGoals(originalGoals);
+				if (nexusStagingSupport) {
+
+					// nexus client tries to load the vocab using the contextClassLoader.
+					ClassLoader originalLoader = Thread.currentThread().getContextClassLoader();
+					ClassLoader newLoader = StageClient.class.getClassLoader();
+					Thread.currentThread().setContextClassLoader(newLoader);
+					try {
+						StageClient client = new StageClient(nexusURL, nexusUser, nexusPassword);
+						MavenModule rootModule = mmSet.getRootModule();
+						// TODO add support for a user supplied comment.
+						StageRepository repo = client.getOpenStageRepositoryForUser(rootModule.getModuleName().groupId,
+						                                                            rootModule.getModuleName().artifactId,
+						                                                            getReleaseVersion(rootModule));
+						if (repo != null) {
+							lstnr.getLogger().append("[M2Release] Closing repository " + repo.getRepositoryId() + " at " + repo.getUrl());
+							client.finishRepository(repo, "Stage for: " + rootModule.getDisplayName());
+						}
+						else {
+							retVal = false;
+							lstnr.fatalError("[M2Release] Could not find nexus repository.");
+							lstnr.getLogger().append("[M2Release] Could not find nexus repository \n");
+						}
+					}
+					catch (RESTLightClientException ex) {
+						lstnr.fatalError("[M2Release] Could not close repository , %s\n", ex.toString());
+						lstnr.getLogger().append("[M2Release] Could not close repository " + ex.toString() + "\n");
+						retVal = false;
+					}
+					finally {
+						Thread.currentThread().setContextClassLoader(originalLoader);
+					}
+				}
 				versions = null;
 				return retVal;
 			}
 		};
 	}
-
 
 	void enableRelease() {
 		doRelease = true;
@@ -150,6 +193,17 @@ public class M2ReleaseBuildWrapper extends BuildWrapper {
 		return sb.toString();
 	}
 
+	private String getReleaseVersion(MavenModule moduleName) {
+		String retVal = null;
+		String key = "-Dproject.rel." + moduleName.getModuleName().toString();
+		retVal = versions.get(key);
+		if (retVal == null) {
+			// we are auto versioning...
+			retVal = moduleName.getVersion().replace("-SNAPSHOT", "");
+		}
+		return retVal;
+	}
+	
 	private MavenModuleSet getModuleSet(AbstractBuild build) {
 		if (build instanceof MavenBuild) {
 			MavenBuild m2Build = (MavenBuild) build;
@@ -173,17 +227,23 @@ public class M2ReleaseBuildWrapper extends BuildWrapper {
 	}
 
 	public static boolean hasReleasePermission(AbstractProject job) {
-		return job.hasPermission(Item.BUILD);
+		return job.hasPermission(DescriptorImpl.CREATE_RELEASE);
 	}
 
 	public static void checkReleasePermission(AbstractProject job) {
-		job.checkPermission(Item.BUILD);
+		job.checkPermission(DescriptorImpl.CREATE_RELEASE);
 	}
 
 	@Extension
 	public static class DescriptorImpl extends BuildWrapperDescriptor {
 		
 		public static final String DEFAULT_RELEASE_GOALS = "-Dresume=false release:prepare release:perform"; //$NON-NLS-1$
+		//public static final String DEFAULT_NEXUS_URL = "http://nexus/nexus/"; //$NON-NLS-1$
+		public static final String DEFAULT_NEXUS_URL = "http://maven-repo.nds.com"; //$NON-NLS-1$
+		public static final String DEFAULT_NEXUS_USER = "deploy_release"; //$NON-NLS-1$
+		public static final String DEFAULT_NEXUS_PASSWORD = "NDSReleaseDeploy"; //$NON-NLS-1$
+		
+		public static final Permission CREATE_RELEASE = new Permission(Job.PERMISSIONS, "Release", Messages._CreateReleasePermission_Description(), Hudson.ADMINISTER);
 		
 		public DescriptorImpl() {
 			super(M2ReleaseBuildWrapper.class);
