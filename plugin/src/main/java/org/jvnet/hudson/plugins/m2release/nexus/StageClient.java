5b80d022b145b68ffcb85fb32d0211a05af12075
teilo <teilo@dev.java.net>
2010-06-08 20:48:23 +0000

    some more code..

--- plugin/src/main/java/org/jvnet/hudson/plugins/m2release/nexus/StageClient.java
+++ plugin/src/main/java/org/jvnet/hudson/plugins/m2release/nexus/StageClient.java
@@ -24,6 +24,7 @@
 package org.jvnet.hudson.plugins.m2release.nexus;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -50,19 +51,16 @@ public class StageClient {
 	private URL nexusURL;
 	private String username;
 	private String password;
-	
+
 	public StageClient(URL nexusURL, String username, String password) {
 		this.nexusURL = nexusURL;
 		this.username = username;
 		this.password = password;
 	}
-	
-	public void testConnection() throws Exception {
-		// test the connection and authorisation.
-	}
-	
+
 	/**
 	 * Get the ID for the Staging repository that holds the specified GAV.
+	 * 
 	 * @param groupId
 	 * @param artifactId
 	 * @param version
@@ -72,54 +70,110 @@ public class StageClient {
 		// to get stage URLs:
 		// ${nexusURL}/service/local/staging/profiles
 		// to check contents:
-		
+
 		return null;
 	}
-	
+
 	/**
 	 * Close the stage id with the specified ID.
+	 * 
 	 * @param stageID
 	 */
 	public void closeStage(String stageID) {
 		// url to close is ${nexusURL}/service/local/staging/profiles/${stageID}/finish
+		// what is the return - just a 200 OK?
 	}
-	
+
 	/**
 	 * Drop the stage with the specified ID.
+	 * 
 	 * @param stageID
 	 */
 	public void dropStage(String stageID) {
-		//  ${nexusURL}/service/local/staging/profiles/${stageID}/drop
+		// ${nexusURL}/service/local/staging/profiles/${stageID}/drop
+		// what is the return - just a 200 OK?
 	}
-	
+
 	/**
 	 * Gets an authentication token from Nexus.
+	 * 
 	 * @return
+	 * @throws IOException 
+	 * @throws XPathExpressionException 
 	 */
-	private String authenticate() {
+	public String checkAuthentication() throws IOException, XPathExpressionException {
+		// ${nexusURL}/service/local/status
+		URL url = new URL(nexusURL.toString() + "/service/local/status");
+		URLConnection conn = url.openConnection();	
+		//addAuthHeader(conn);
+		InputSource inputSource = new InputSource(conn.getInputStream());
+		/*
+		 * check for the following permissions:
+		 * nexus:stagingprofiles
+		 * nexus:stagingfinish
+		 * nexus:stagingpromote
+		 * nexus:stagingdrop
+		 */
+		boolean profilesPerm, finishPerm, closePerm, dropPerm;
+		
+		XPath xpath = XPathFactory.newInstance().newXPath();
+		String expression = "//clientPermissions/permissions/permission";
+		NodeList nodes = (NodeList) xpath.evaluate(expression, inputSource, XPathConstants.NODESET);
+		for (int i = 0; i < nodes.getLength(); i++) {
+			Node node = nodes.item(i);
+			// permission has id and value.
+			// check if id is one listed above then value != 0
+			NodeList innerNode = node.getChildNodes();
+			for (int j = 0; j < innerNode.getLength(); j++) {
+				System.out.println(innerNode.item(j).getTextContent().trim());
+			}
+		}
 		return null;
 	}
-	
+
 	public List<String> getOpenStageIDs() throws IOException, XPathExpressionException {
 		List<String> openStages = new ArrayList<String>();
 		URL url = new URL(nexusURL.toString() + "/service/local/staging/profiles");
-		String expression = "/stagingProfiles/data/stagingProfile/stagingRepositoryIds/string/text()";
-		//String expression = "string";
+		String expression = "//stagingRepositoryIds/string/text()";
+		// String expression = "string";
 		XPath xpath = XPathFactory.newInstance().newXPath();
-		InputSource inputSource = new InputSource(url.toString());
+		URLConnection conn = url.openConnection();	
+		addAuthHeader(conn);
+		InputSource inputSource = new InputSource(conn.getInputStream());
 		NodeList nodes = (NodeList) xpath.evaluate(expression, inputSource, XPathConstants.NODESET);
-		for (int i=0; i < nodes.getLength(); i++) {
+		for (int i = 0; i < nodes.getLength(); i++) {
 			Node node = nodes.item(i);
 			openStages.add(node.getNodeValue());
 		}
-		
 		return openStages;
 	}
-	
-	public boolean checkStageForGAV(String stageID, String group, String artifact, String version) {
-		// do we always know the version???
-		// to browse an open repo  /service/local/repositories/${stageID}/content/...
-		return false;
+
+	@SuppressWarnings("restriction")
+  private void addAuthHeader(URLConnection conn) {
+		 String auth = username + ":" + password;
+		 // this is bad - should look at using some other BASE64 encoder
+		 // and perhaps a different HTTPClient?
+		 String encodedAuth = new sun.misc.BASE64Encoder().encode (auth.getBytes());
+		 conn.setRequestProperty ("Authorization", "Basic " + encodedAuth);
 	}
 	
+	public boolean checkStageForGAV(String stageID, String group, String artifact, String version)
+	    throws MalformedURLException {
+		// do we always know the version???
+		// to browse an open repo /service/local/repositories/${stageID}/content/...
+		boolean found = false;
+		URL url = new URL(nexusURL.toString() + "/service/local/repositories/" + stageID + group.replace('.', '/') + '/');
+		try {
+			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+			addAuthHeader(conn);
+			conn.getResponseCode();
+			url.getContent();
+			found = true;
+		}
+		catch (IOException ex) {
+			// some debug message - but likely to happen for a 404.
+		}
+		return found;
+	}
+
 }
--- plugin/src/test/java/org/jvnet/hudson/plugins/m2release/nexus/StageTest.java
+++ plugin/src/test/java/org/jvnet/hudson/plugins/m2release/nexus/StageTest.java
@@ -1,10 +1,7 @@
 package org.jvnet.hudson.plugins.m2release.nexus;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.List;
-
-import javax.xml.xpath.XPathExpressionException;
 
 import junit.framework.Assert;
 
@@ -13,9 +10,30 @@ import org.junit.Test;
 public class StageTest {
 
 	@Test
-	public void testStage() throws XPathExpressionException, IOException {
-		URL testURL = StageTest.class.getResource("stageTest");
-		StageClient client = new StageClient(testURL, null, null);
+	public void testValidAuth() throws Exception {
+		// TODO start an embedded server instead to server these files so we also test http auth access?
+		//URL testURL = StageTest.class.getResource("stageTest");
+		URL testURL = new URL("http://localhost:8081/nexus");
+		StageClient client = new StageClient(testURL, "admin", "admin123");
+		client.checkAuthentication();
+	}
+
+	@Test(expected=Exception.class)
+	public void testInvalidAuth() throws Exception {
+		// TODO start an embedded server instead to server these files so we also test http auth access?
+		//URL testURL = StageTest.class.getResource("stageTest");
+		URL testURL = new URL("http://localhost:8081/nexus");
+		StageClient client = new StageClient(testURL, "bob", "jones");
+		client.checkAuthentication();
+	}
+
+	
+	@Test
+	public void testStage() throws Exception {
+		// TODO start an embedded server instead to server these files so we also test http auth access?
+		//URL testURL = StageTest.class.getResource("stageTest");
+		URL testURL = new URL("http://localhost:8081/nexus");
+		StageClient client = new StageClient(testURL, "admin", "admin123");
 		List<String> stages = client.getOpenStageIDs();
 		Assert.assertEquals("incorrect number of stages returned", 2, stages.size());
 		Assert.assertEquals("Incorrect stage returned", "test-001", stages.get(0));
