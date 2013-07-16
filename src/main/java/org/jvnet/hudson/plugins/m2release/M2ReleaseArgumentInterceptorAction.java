4382a8966644a41467247b46ea116d78f78d9901
robertkleinschmager <robert@kleinschmager.net>
2013-07-16 02:14:06 -0400

    fix for JENKINS-10914, using ArgumentInterceptorAction to filter the incremental build arguments

--- src/main/java/org/jvnet/hudson/plugins/m2release/M2ReleaseArgumentInterceptorAction.java
+++ src/main/java/org/jvnet/hudson/plugins/m2release/M2ReleaseArgumentInterceptorAction.java
@@ -23,6 +23,13 @@
  */
 package org.jvnet.hudson.plugins.m2release;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+
 import hudson.maven.MavenArgumentInterceptorAction;
 import hudson.maven.MavenModuleSetBuild;
 import hudson.util.ArgumentListBuilder;
@@ -61,7 +68,77 @@ public class M2ReleaseArgumentInterceptorAction implements MavenArgumentIntercep
 	}
 
 	public ArgumentListBuilder intercept(ArgumentListBuilder mavenargs, MavenModuleSetBuild build) {
-		return null;
+		
+		ArgumentListBuilder returnListBuilder = new ArgumentListBuilder();
+		
+		if (containsJenkinsIncrementalBuildArguments(mavenargs))
+		{
+			returnListBuilder = removeAllIncrementalBuildArguments(mavenargs.clone());
+		} else
+		{
+			returnListBuilder = mavenargs.clone();
+		}
+		
+		return returnListBuilder;
 	}
+	
+	private boolean containsJenkinsIncrementalBuildArguments(ArgumentListBuilder mavenargs)
+	{
+		int amdIndex = mavenargs.toList().indexOf("-amd");		
+		return amdIndex != -1;
+	}
+	
+	private ArgumentListBuilder removeAllIncrementalBuildArguments(
+			ArgumentListBuilder mavenargs) {
+		
+		// remove the three elements which was added by MavenModuleSetBuild
+		// -amd
+		// -pl
+		// <list of modules>
+		
+		ArgumentListBuilder returnListBuilder = new ArgumentListBuilder();
+		
+		int amdIndex = mavenargs.toList().indexOf("-amd");
+		
+		ensureArgumentsAndMaskHaveSaveSize(mavenargs);
+
+		boolean[] maskArray = mavenargs.toMaskArray();
+		ArrayList<Boolean> maskList = Lists.newArrayList();
+		for (boolean b : maskArray) {
+			maskList.add(Boolean.valueOf(b));
+		}
+		
+		List<String> oldArgumentList = mavenargs.toList();
+
+		// as List.remove is shifting all elements, we can reuse the index
+		String removedAmd = oldArgumentList.remove(amdIndex);
+		Preconditions.checkArgument("-amd".equals(removedAmd));
+		maskList.remove(amdIndex);
+		
+		String removedPl = oldArgumentList.remove(amdIndex);
+		Preconditions.checkArgument("-pl".equals(removedPl));			
+		maskList.remove(amdIndex);
+		
+		oldArgumentList.remove(amdIndex);
+		maskList.remove(amdIndex);		
+		
+		// rebuild
+		for (int i=0; i < oldArgumentList.size() ; i++) {
+				returnListBuilder.add(oldArgumentList.get(i), maskList.get(i).booleanValue());
+		}
+
+		ensureArgumentsAndMaskHaveSaveSize(returnListBuilder);
+		
+		return returnListBuilder;
+	}
+	
+	private void ensureArgumentsAndMaskHaveSaveSize(ArgumentListBuilder alb) {
+		if (alb.toList().size() != alb.toMaskArray().length)
+		{
+			throw new RuntimeException("could not intercept argument list: ArgumentList and Mask are out of sync ");
+		}
+	}
+	
+	
 
 }
