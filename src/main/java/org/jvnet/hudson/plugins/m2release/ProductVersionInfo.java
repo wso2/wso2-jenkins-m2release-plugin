216ad27af69cf74fa4db8c5f78ba92874dcd5034
ThilinaManamgoda <maanafunedu@gmail.com>
2017-12-19 23:07:09 +0530

    fix version number 9>

--- src/main/java/org/jvnet/hudson/plugins/m2release/ProductVersionInfo.java
+++ src/main/java/org/jvnet/hudson/plugins/m2release/ProductVersionInfo.java
@@ -8,14 +8,18 @@ import java.util.regex.Pattern;
 
 public class ProductVersionInfo extends DefaultVersionInfo{
 
-    public final static Pattern PRODUCT_VERSION_PATTERN = Pattern.compile("(.*)((([-_]update)([1-9]+))([-_]SNAPSHOT))$");
-
-
+    public final static Pattern PRODUCT_VERSION_PATTERN = Pattern.compile("(.*)((([-_]update)([1-9]+)(\\d*))([-_]SNAPSHOT))$");
     public ProductVersionInfo(String version) throws VersionParseException {
         super(version);
         Matcher matcher = PRODUCT_VERSION_PATTERN.matcher(version);
         if (!matcher.matches()){
             throw new VersionParseException("Product version is not compatible");
         }
+    }
+
+
+    public static void main(String[] args) {
+        Matcher m=PRODUCT_VERSION_PATTERN.matcher("1.0.32-update9-SNAPSHOT");
+        System.out.println(m.matches());
     }
 }
