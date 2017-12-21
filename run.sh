7e6f41064f979e8333a7a6239b7bfe9d6f51d6b3
ThilinaManamgoda <maanafunedu@gmail.com>
2017-12-21 13:46:24 +0530

    removed run script

--- run.sh
+++
@@ -1,6 +0,0 @@
-mvn clean install -Dmaven.test.skip
-sudo service jenkins stop
-sudo rm -rf /var/lib/jenkins/plugins/m2release*
-sudo cp target/m2release.hpi /var/lib/jenkins/plugins/
-sudo service jenkins start
-
